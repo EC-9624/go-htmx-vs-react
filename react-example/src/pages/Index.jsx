f62e9f7d23d2f15aff1504213e89917119e0d50f
eakudompong.chanoknan <ceguantz96@outlook.com>
2025-01-24 13:33:05 +0900

    fix img path add readme.md

--- react-example/src/pages/Index.jsx
+++ react-example/src/pages/Index.jsx
@@ -27,7 +27,7 @@ export default function Index() {
                 <div className="flex-shrink-0">
                   <img
                     className="h-12 w-12"
-                    src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg"
+                    src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.d52e9897.svg"
                     alt="your company"
                   />
                 </div>
---
+++ readme.md
@@ -0,0 +1,16 @@
+## requirements
+
+- Go version go1.23.2 以上
+- NodeJS v22.13.0
+
+### GO-HTMX アプリの起動方法
+
+```
+cd ./go-htmx-example && air
+```
+
+### React アプリの起動方法
+
+```
+cd ./react-example && npm install && npm run dev
+```
